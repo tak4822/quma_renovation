6f1cfd9c093798f2d40b769ed4bb8f8f81ece360
tak <tkc4822@gmail.com>
2019-06-11 22:58:19 +0900

    set up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,13 +7,13 @@
     <div class="wrap container" role="document">
       <div class="content">
         <main class="main">
-          @yield('content')
+          {{-- @yield('content') --}}
         </main>
-        @if (App\display_sidebar())
+        {{-- @if (App\display_sidebar())
           <aside class="sidebar">
             @include('partials.sidebar')
           </aside>
-        @endif
+        @endif --}}
       </div>
     </div>
     @php(do_action('get_footer'))
