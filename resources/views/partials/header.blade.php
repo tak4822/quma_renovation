5c3b9b384c14c429747790e2353bf097ebca7add
tak <tkc4822@gmail.com>
2019-06-14 22:51:51 +0900

    add nav

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,12 +1,24 @@
-<header class="banner">
+<header class="global-nav">
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">quma</a>
-    <div class="nav-container">
+
+    <div class="nav-wrapper">
+      <a class="logo" href="{{ home_url('/') }}"></a>
       <nav class="nav-primary">
         @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
         @endif
       </nav>
+    </div>
+
+    <div class="nav-action">
+      <div class="line btn">
+        <img src="@asset('images/ico_line.svg')" alt="" class="ico">
+        <p>LINE@相談</p>
+      </div>
+      <div class="tel btn">
+        <img src="@asset('images/ico_tel.svg')" alt="" class="ico">
+        <p>03-6805-0696</p>
+      </div>
     </div>
   </div>
 </header>
