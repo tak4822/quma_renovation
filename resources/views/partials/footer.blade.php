bc334597b02d3d5dc837ba65d45644dc5b234e47
tak <tkc4822@gmail.com>
2019-07-25 16:23:38 +0900

    fix header menue position

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,49 +1,41 @@
 <footer class="footer">
   <div class="content">
     <p class="greeting">Say Hello</p>
+
     <div class="contact-container">
-      <div class="contact-action">
-        <img class="triangle" src="@asset('images/mail_triangle.svg')" alt="">
-        <p class="text">お問い合わせ</p>
+      <div class="left">
+        <div class="contact-action">
+          <img class="triangle" src="@asset('images/mail_triangle.svg')" alt="">
+          <p class="text">お問い合わせ</p>
+        </div>
+
+        <div class="contact-info">
+          <ul class="info-list">
+            <li class="info tel">
+              <img class="icon" src="@asset('images/footer_tel.svg')" alt="">
+              <p class="text">03-6805-0696</p>
+            </li>
+            <li class="info mail">
+              <img class="icon" src="@asset('images/footer_mail.svg')" alt="">
+              <p class="text">info@quma.co.jp</p>
+            </li>
+            <li class="info address">
+              <img class="icon" src="@asset('images/footer_address.svg')" alt="">
+              <p class="text">〒150-0011<br />東京都渋谷区東3-6-18</p>
+            </li>
+            <li class="info time">
+              <img class="icon" src="@asset('images/footer_time.svg')" alt="">
+              <p class="text">9:00-17:00 日曜定休日</p>
+            </li>
+          </ul>
+        </div>
       </div>
 
-      <div class="contact-info">
-        <ul class="info-list">
-          <li class="info tel">
-            <img class="icon" src="@asset('images/footer_tel.svg')" alt="">
-            <p class="text">03-6805-0696</p>
-          </li>
-          <li class="info mail">
-            <img class="icon" src="@asset('images/footer_mail.svg')" alt="">
-            <p class="text">info@quma.co.jp</p>
-          </li>
-          <li class="info address">
-            <img class="icon" src="@asset('images/footer_address.svg')" alt="">
-            <p class="text">〒150-0011<br />東京都渋谷区東3-6-18</p>
-          </li>
-          <li class="info time">
-            <img class="icon" src="@asset('images/footer_time.svg')" alt="">
-            <p class="text">9:00-17:00 日曜定休日</p>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <div class="line-container">
-    <div class="line-wrapper">
-      <div class="bg">
-        <div class="circle"></div>
-        <div class="square"></div>
-      </div>
-      <div class="contents">
-        <img src="@asset('images/LINE@_logo.svg')" alt="">
-        <div class="text-container">
-          <p class="title">かんたん！Line＠相談</p>
-          <p class="text">リノベーションって何ができるの？私の条件だったら賃貸とリノベってどっちが良いの！？なんでも気軽にLINEで相談！</p>
-          <div class="line-btn">
-            <p class="line-btn-text">Lineで相談する</p>
-          </div>
+      <div class="copyright-container">
+        <div class="logo">
+          <img src="@asset('images/quma_logo.svg')" alt="">
         </div>
+        <p class="right">Copyright ©QUMA All rights reserved</p>
       </div>
     </div>
   </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,20 +1,18 @@
 <header class="global-nav">
   <div class="container">
+    <nav class="nav-primary">
+      @if (has_nav_menu('primary_navigation'))
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+      @endif
+    </nav>
 
-    <div class="nav-wrapper">
-      <a class="logo" href="{{ home_url('/') }}"></a>
-      <nav class="nav-primary">
-        @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-        @endif
-      </nav>
-    </div>
+    <a class="logo" href="{{ home_url('/') }}"></a>
 
     <div class="nav-action">
-      <div class="line btn">
+      {{-- <div class="line btn">
         <img src="@asset('images/ico_line.svg')" alt="" class="ico">
         <p>LINE@相談</p>
-      </div>
+      </div> --}}
       <div class="tel btn">
         <img src="@asset('images/ico_tel.svg')" alt="" class="ico">
         <p>03-6805-0696</p>
