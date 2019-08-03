a5473a06de7e2f916063842fdf5afb99731530a5
tak <tkc4822@gmail.com>
2019-08-03 11:58:34 +0900

    added page interview

---
+++ resources/views/components/interview-list.blade.php
@@ -0,0 +1,19 @@
+<div class="inteview-list">
+  <div class="contents">
+    <div class="number">{{ $number }}</div>
+    <div class="text-wrapper">
+      <p class="subtitle">{{ $subtitle}}</p>
+      <p class="title">{{ $title}}</p>
+      <div class="outline">
+        <p class="name">{{ $name }}</p>
+        <div class="slash"></div>
+        <p class="family">{{ $family }}</p>
+        <div class="slash"></div>
+        <p class="address">{{ $address }}</p>
+      </div>
+    </div>
+  </div>
+  <div class="pictures">
+    {{ $img }}
+  </div>
+</div>
---
+++ resources/views/page-interviews.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="interviews-page pages-container">
+
+  @component('components.breadcrums')
+  @slot('page')Interviews @endslot
+  @slot('text')リノベ、どうでした？@endslot
+  @endcomponent
+
+  <section class="interviews-container">
+    @component('components.page-section-header')
+    @slot('title')リノベ体験インタビュー@endslot
+    @slot('sub')QUMAで住まいつくりのお手伝いをさせて頂いた方々に、QUMAとのリノベーションの感想を包み隠さずお話しして頂きました。参考になる話があるかも！？@endslot
+    @slot('img')
+    <img src="@asset('images/vision.jpg')" alt="">
+    @endslot
+    @endcomponent
+
+
+
+    <div class="interviews-list regular-container">
+
+      @component('components.interview-list')
+      @slot('number')002 @endslot
+      @slot('subtitle')アウトドア好きでアクティブな人の部屋って？@endslot
+      @slot('title')ひろびろ収納と無垢フローリングの落ち着く部屋づくり@endslot
+      @slot('name')F様邸 @endslot
+      @slot('family')SINGLE @endslot
+      @slot('address')吉祥寺 @endslot
+      @slot('img')
+      <img src="@asset('images/top_fake.jpg')" alt="">
+      @endslot
+      @endcomponent
+
+      @component('components.interview-list')
+      @slot('number')001 @endslot
+      @slot('subtitle')アウトドア好きでアクティブな人の部屋って？@endslot
+      @slot('title')ひろびろ収納と無垢フローリングの落ち着く部屋づくり@endslot
+      @slot('name')F様邸 @endslot
+      @slot('family')SINGLE @endslot
+      @slot('address')吉祥寺 @endslot
+      @slot('img')
+      <img src="@asset('images/top_fake.jpg')" alt="">
+      @endslot
+      @endcomponent
+    </div>
+  </section>
+
+</div>
+@endsection
