0e87ce702571ce07ade1b515714fdc64f462099b
tak <tkc4822@gmail.com>
2019-08-13 15:12:09 +0900

    adjusted style of hamburger and interviews list

--- resources/assets/scripts/modules/hamburger.js
+++ resources/assets/scripts/modules/hamburger.js
@@ -1,29 +1,36 @@
+function close() {
+  $('.nav-primary')
+    .removeClass('active')
+    .find("li")
+    .removeClass('active');
+  $('.global-nav .container').removeClass("is-active");
+  $('.hamburger').removeClass('is-active');
+}
+
+function open() {
+  $('.hamburger').addClass('is-active');
+  $('.global-nav .container').addClass("is-active");
+  $('.nav-primary')
+    .addClass("active")
+    .delay(500)
+    .find("li")
+    .each(function (i) {
+      $(this).delay(100 * i).queue(function (next) {
+        $(this).addClass("active");
+        next();
+      })
+    });
+}
+
 export default function () {
   $('.hamburger').on('click', function () {
     if ($(this).hasClass('is-active')) {
-      $('.nav-primary')
-        .removeClass('active')
-        .find("li")
-        .removeClass('active');
-      $('.global-nav .container').removeClass("is-active");
-      $(this).removeClass('is-active');
+      close();
     } else {
-      $(this).addClass('is-active');
-      $('.global-nav .container').addClass("is-active");
-      $('.nav-primary')
-        .addClass("active")
-        .delay(500)
-        .find("li")
-        .each(function (i) {
-          $(this).delay(100 * i).queue(function (next) {
-            $(this).addClass("active");
-            next();
-          })
-        });
+      open();
     }
   })
-  $('.nav-primary a').click(function () {
-    $('.hamburger').removeClass('is-active');
-    $('.nav-primary').removeClass('active');
+  $('.global-nav a').click(function () {
+    close();
   })
 }
