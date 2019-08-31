10fff6911f15943210907085f8865c696ecfa9be
tak <tkc4822@gmail.com>
2019-08-31 22:06:36 +0900

    modified style and make it ssl

--- resources/views/layouts/front-works.blade.php
+++ resources/views/layouts/front-works.blade.php
@@ -11,8 +11,8 @@
     <a href="{{ get_permalink() }}" class="pick-container {{ $count_works_posts_picked === 0 ? "first" : "second"}}">
       <div class="circle"></div>
       <div class="thumb">
-        <img class="wow {{ $count_works_posts_picked === 0 ? "slideInRight" : "slideInLeft"}}" src="
-          {{ get_the_post_thumbnail_url() }}" alt="">
+        <img class="thumb-img wow {{ $count_works_posts_picked === 0 ? "slideInRight" : "slideInLeft"}}" src="
+          {{ get_the_post_thumbnail_url() }}" alt="{{ get_the_title() }}">
       </div>
       <div class="contents wow late fadeIn">
         <h4 class="title">{{ get_the_title() }}</h4>
